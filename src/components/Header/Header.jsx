9461fa71090eaea11e6fe0afec8060c0112b6e18
[rezaeiar] <rezaeiar0@gmail.com>
2023-10-03 12:55:07 +0330

    feat: Create header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,66 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { NavLink } from 'react-router-dom'
+import NavBar from '../NavBar/NavBar'
 
 export default function Header() {
     return (
-        <div className='container flex items-center justify-between pt-[30px]'>
-            <div className="flex items-center gap-x-2.5">
-                <Link to='/' className="flex items-center justify-center shadow-normal lg:h-[55px] lg:w-[55px] h-12 w-12 bg-white rounded-xl lg:rounded-2xl">
-                    <img src="./images/mainweb/3D/Sec1/path28.svg" className='h-7 lg:h-[31px]' alt="" />
-                </Link>
-                <div className="flex flex-col">
-                    <Link to='/' className='text-base lg:text-lg/5 font-Ray-ExtraBold text-main-blue-web'>
-                        LovelCode
-                    </Link>
-                    <Link to='/' className='text-xs lg:text-sm/4 font-Ray-Black text-main-gray-text-web'>
+        <>
+            <NavBar />
+            <div className="container flex flex-col items-center">
+                <h4 className='text-xl sm:text-3xl lg:text-[40px] font-Ray-ExtraBold text-main-dark-text-web mb-2.5 text-center'>
+                    آژانــس حرفــه ای دیــــجیتـــــال مــــــارکـــتـیـنـگ
+                    <span className='text-main-blue-web inline-block'>
                         لاول کد
-                    </Link>
+                    </span>
+                </h4>
+                <div className="flex items-center gap-x-10 lg:gap-x-12 mb-2 sm:mb-8">
+                    <img src="/images/mainweb/3D/Sec1/2.png" className='hidden sm:block h-2 lg:h-3' alt="" />
+                    <span className='text-main-green-web text-base sm:text-xl lg:text-2xl font-Ray-Bold text-center'>
+                        در کنار راه اندازی کسب و کار مجازی شما هستیم :)
+                    </span>
+                    <img src="/images/mainweb/3D/Sec1/2.png" className='hidden sm:block h-2 lg:h-3' alt="" />
+                </div>
+                <div className="mb-6 sm:mb-8">
+                    <img src="/images/mainweb/3D/Sec1/1.png" className='max-h-[320px] sm:max-h-[400px] lg:max-h-[554px]' alt="" />
+                </div>
+                <div className="mb-5 sm:mb-10 md:mb-20">
+                    <img src="/images/mainweb/3D/Sec1/3.png" className='h-16 sm:h-[70px]' alt="" />
                 </div>
             </div>
-            <ul className="hidden md:flex text-xs lg:text-lg text-main-gray-text-web font-Ray-Bold gap-x-6 lg:gap-x-[34px]">
-                <li className='hover:text-main-blue-web transition-colors'>
-                    <NavLink to='/' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
-                        صفحه اصلی
-                    </NavLink>
-                </li>
-                <li className='hover:text-main-blue-web transition-colors'>
-                    <NavLink to='/portfolio' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
-                        نمونه کارها
-                    </NavLink>
-                </li>
-                <li className='hover:text-main-blue-web transition-colors'>
-                    <NavLink to='/s' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
-                        تعرفه طراحی سایت
-                    </NavLink>
-                </li>
-                <li className='hover:text-main-blue-web transition-colors'>
-                    <NavLink to='/d' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
-                        وبلاگ
-                    </NavLink>
-                </li>
-                <li className='hover:text-main-blue-web transition-colors'>
-                    <NavLink to='/f' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
-                        درباره ما
-                    </NavLink>
-                </li>
-                <li className='hover:text-main-blue-web transition-colors'>
-                    <NavLink to='/g' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
-                        تماس با ما
-                    </NavLink>
-                </li>
-            </ul>
-            <div className="hidden md:flex items-center gap-x-2 lg:gap-x-6">
-                <Link to='/' className='text-main-blue-web px-4 py-2 text-sm lg:text-base'>
-                    ورود
-                </Link>
-                <Link to='/' className='w-20 h-10 lg:w-24 lg:h-12 flex justify-center items-center lg:rounded-xl rounded-lg text-sm lg:text-base bg-main-blue-web text-white'>
-                    ثبت نام
-                </Link>
-            </div>
-            <div className='block md:hidden text-xl cursor-pointer'>
-                <i class="bi bi-list"></i>
-            </div>
-        </div>
+        </>
     )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+
+export default function NavBar() {
+    return (
+        <div className='container flex items-center justify-between pt-[30px] mb-10 sm:mb-16 lg:mb-24'>
+            <div className="flex items-center gap-x-2.5">
+                <Link to='/' className="flex items-center justify-center shadow-normal lg:h-[55px] lg:w-[55px] h-12 w-12 bg-white rounded-xl lg:rounded-2xl">
+                    <img src="./images/mainweb/3D/Sec1/path28.svg" className='h-7 lg:h-[31px]' alt="" />
+                </Link>
+                <div className="flex flex-col">
+                    <Link to='/' className='text-base lg:text-lg/5 font-Ray-ExtraBold text-main-blue-web'>
+                        LovelCode
+                    </Link>
+                    <Link to='/' className='text-xs lg:text-sm/4 font-Ray-Black text-main-gray-text-web'>
+                        لاول کد
+                    </Link>
+                </div>
+            </div>
+            <ul className="hidden md:flex text-xs lg:text-lg text-main-gray-text-web font-Ray-Bold gap-x-6 lg:gap-x-[34px]">
+                <li className='hover:text-main-blue-web transition-colors'>
+                    <NavLink to='/' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
+                        صفحه اصلی
+                    </NavLink>
+                </li>
+                <li className='hover:text-main-blue-web transition-colors'>
+                    <NavLink to='/portfolio' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
+                        نمونه کارها
+                    </NavLink>
+                </li>
+                <li className='hover:text-main-blue-web transition-colors'>
+                    <NavLink to='/s' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
+                        تعرفه طراحی سایت
+                    </NavLink>
+                </li>
+                <li className='hover:text-main-blue-web transition-colors'>
+                    <NavLink to='/d' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
+                        وبلاگ
+                    </NavLink>
+                </li>
+                <li className='hover:text-main-blue-web transition-colors'>
+                    <NavLink to='/f' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
+                        درباره ما
+                    </NavLink>
+                </li>
+                <li className='hover:text-main-blue-web transition-colors'>
+                    <NavLink to='/g' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
+                        تماس با ما
+                    </NavLink>
+                </li>
+            </ul>
+            <div className="hidden md:flex items-center gap-x-2 lg:gap-x-6">
+                <Link to='/' className='text-main-blue-web px-4 py-2 text-sm lg:text-base'>
+                    ورود
+                </Link>
+                <Link to='/' className='w-20 h-10 lg:w-24 lg:h-12 flex justify-center items-center lg:rounded-xl rounded-lg text-sm lg:text-base bg-main-blue-web text-white'>
+                    ثبت نام
+                </Link>
+            </div>
+            <div className='block md:hidden text-xl cursor-pointer'>
+                <i class="bi bi-list"></i>
+            </div>
+        </div>
+    )
+}
