628bb69e8602c7f52ca885b14c872d5f6be956cb
mohammadreza <spd.mohammadreza@gmail.com>
2023-10-03 13:23:58 -0700

    add backend mohammadreza

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Footer() {
+  return (
+    <>
+      <div className="bg-gradient-to-r from-main-blue-web to-main-violet-web pt-12 pb-5 text-white rounded-t-[50px]">
+        
+      </div>
+    </>
+  );
+}
