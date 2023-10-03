9c7639b05600e2481540235a6b935642b5083894
[rezaeiar] <rezaeiar0@gmail.com>
2023-10-03 12:18:39 +0330

    fix: fix navbar responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,19 +6,19 @@ export default function Header() {
     return (
         <div className='container flex items-center justify-between pt-[30px]'>
             <div className="flex items-center gap-x-2.5">
-                <Link to='/' className="flex items-center justify-center shadow-normal h-[55px] w-[55px] bg-white rounded-2xl">
-                    <img src="./images/mainweb/3D/Sec1/path28.svg" className='h-[31px]' alt="" />
+                <Link to='/' className="flex items-center justify-center shadow-normal lg:h-[55px] lg:w-[55px] h-12 w-12 bg-white rounded-xl lg:rounded-2xl">
+                    <img src="./images/mainweb/3D/Sec1/path28.svg" className='h-7 lg:h-[31px]' alt="" />
                 </Link>
                 <div className="flex flex-col">
-                    <Link to='/' className='text-lg/5 font-Ray-ExtraBold text-main-blue-web'>
+                    <Link to='/' className='text-base lg:text-lg/5 font-Ray-ExtraBold text-main-blue-web'>
                         LovelCode
                     </Link>
-                    <Link to='/' className='text-sm/4 font-Ray-Black text-main-gray-text-web'>
+                    <Link to='/' className='text-xs lg:text-sm/4 font-Ray-Black text-main-gray-text-web'>
                         لاول کد
                     </Link>
                 </div>
             </div>
-            <ul className="flex text-lg text-main-gray-text-web font-Ray-Bold gap-x-[34px]">
+            <ul className="hidden md:flex text-xs lg:text-lg text-main-gray-text-web font-Ray-Bold gap-x-6 lg:gap-x-[34px]">
                 <li className='hover:text-main-blue-web transition-colors'>
                     <NavLink to='/' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
                         صفحه اصلی
@@ -50,13 +50,16 @@ export default function Header() {
                     </NavLink>
                 </li>
             </ul>
-            <div className="flex items-center gap-x-6">
-                <Link to='/' className='text-main-blue-web px-4 py-2'>
+            <div className="hidden md:flex items-center gap-x-2 lg:gap-x-6">
+                <Link to='/' className='text-main-blue-web px-4 py-2 text-sm lg:text-base'>
                     ورود
                 </Link>
-                <Link to='/' className='w-24 h-12 flex justify-center items-center rounded-xl bg-main-blue-web text-white'>
+                <Link to='/' className='w-20 h-10 lg:w-24 lg:h-12 flex justify-center items-center lg:rounded-xl rounded-lg text-sm lg:text-base bg-main-blue-web text-white'>
                     ثبت نام
                 </Link>
+            </div>
+            <div className='block md:hidden text-xl cursor-pointer'>
+                <i class="bi bi-list"></i>
             </div>
         </div>
     )
