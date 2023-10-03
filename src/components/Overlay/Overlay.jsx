65a6e50792906ed620dfc477c5d36797441abfdb
[rezaeiar] <rezaeiar0@gmail.com>
2023-10-03 20:24:01 +0330

    fix: fix overlay

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -3,11 +3,10 @@ import MenuContext from '../../context/MenuContext'
 
 export default function Overlay() {
     const menuContext = useContext(MenuContext)
-    console.log(menuContext.menuDisplay);
     const closeMenu = () => {
         menuContext.menuDisplayHandler(false)
     }
     return (
-        <div onClick={closeMenu} className={`fixed inset-0 bg-black/20 z-20 transition-all ${menuContext.menuDisplay ? 'visible opacity-100' : 'invisible opacity-0'}`}></div>
+        <div onClick={closeMenu} className={`fixed backdrop-blur-sm inset-0 bg-black/20 z-20 transition-all ${menuContext.menuDisplay ? 'visible opacity-100' : 'invisible opacity-0'}`}></div>
     )
 }
