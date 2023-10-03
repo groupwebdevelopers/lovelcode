e24ab009c0f1535d2fb39073669439ed428eec9c
[rezaeiar] <rezaeiar0@gmail.com>
2023-10-03 23:31:58 +0330

    feat: create portfolio header

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import NavBar from '../../components/NavBar/NavBar'
 
 export default function Portfolio() {
     return (
-        <div>Portfolio</div>
+        <div className='relative'>
+            <NavBar />
+        </div>
     )
 }
