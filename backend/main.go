bd269c45a20015309b44930e162c21351016e0b2
yaghoubi-mn <yaghoubi.mn@gmail.com>
2023-10-04 19:41:38 +0330

    add html template

--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,22 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 	"lovelode/router"
 )
 
 
 func main(){
-	app:= fiber.New()
+
+	engine := html.New("../views", ".html")
+	
+	app:= fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	
+
+
 	//add html views
 	database.Setup()
 	//settings.Setup()
