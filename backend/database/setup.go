0c5e6c302e3e43fa7fd00b86524d87d21f546f0e
yaghoubi-mn <yaghoubi.mn@gmail.com>
2023-10-04 21:21:16 +0330

    update file

--- backend/database/setup.go
+++ backend/database/setup.go
@@ -2,9 +2,19 @@ package database
 
 import (
 	"gorm.io/gorm"
+	"gorm.io/dirver/mysql"
 )
 
+var DB *gorm.DB
+
 func Setup() error{
-	//TODO
+	dsn := ""
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err!=nil{
+		return err
+	}
+	DB = &db
+	db.AutoMigrate(&User{})
+
 	return nil
 }
