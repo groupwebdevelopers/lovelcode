27eb10b8801e37423c86a57e0873442b5fa0d42c
yaghoubi-mn <yaghoubi.mn@gmail.com>
2023-10-04 20:42:41 +0330

    create file and create function

---
+++ backend/handlers/grouphandlers.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func ApiOnly(c *fiber.Ctx){
+	api, ok := c.GetReqHeaders()["api"]
+	if api=="true" && ok==true{
+		return c.Next()
+	}
+	return c.Render("index", fiber.Map{})
+}
