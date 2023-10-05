6bc1fa201c85394a5ae6177e5da297d7e652891a
yaghoubi-mn <yaghoubi.mn@gmail.com>
2023-10-05 17:42:26 +0330

    fix main

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
@@ -9,7 +10,7 @@ import (
 
 	"lovelcode/router"
 	"lovelcode/database"
-	
+"fmt"
 )
 
 
@@ -33,7 +34,21 @@ func main(){
 	//	log.Fatal("can't connect to database")
 	//}
 	//settings.Setup()
-	app.Static("/", "../frontend")
+	app.Static("/", "../frontend", fiber.Static{ModifyResponse: func(c *fiber.Ctx) error{
+		if strings.Contains(c.GetRespHeader("Content-Type"), "text/plain"){
+			c.Response().Header.Set("Content-Type", "application/javascript")
+			c.Response().Header.Set("Connection", "keep-alive")
+			c.Response().Header.Set("Keep-Alive", "timeout=5")
+			c.Response().Header.Set("Cache-Control", "no-cache")
+			fmt.Println(c.OriginalURL())
+		}
+		fmt.Println(c.GetRespHeader("Content-Type"))
+		return nil
+	}})
+	//app.Get("/src/main.jsx", func (c *fiber.Ctx) error{
+	//	c.Re
+	//})
+	
 	router.Route(app)
 	log.Fatal(app.Listen(":8000"))
 }
