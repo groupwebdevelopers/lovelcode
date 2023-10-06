1e13e2180f4b414aeb3f0f8f4ba8630d16030272
azizollah <azizollahpayandeh1@gmail.com>
2023-10-06 12:47:00 +0330

    finish

--- frontend/src/components/Features/Features.jsx
+++ frontend/src/components/Features/Features.jsx
@@ -1,124 +1,101 @@
 import React from "react";
 
+function FeatureCard({ imageSrc, title }) {
+  return (
+    <div className="bg-white md:w-[280px]  md:h-[260px] w-[190px] h-[200px] text-center rounded-2xl">
+      <img
+        src={imageSrc}
+        alt=""
+        className="md:w-[200px] w-[140px] md:mr-10 mr-[25px]"
+      />
+      <h1 className="font-Ray-ExtraBold md:text-xl text-lg">{title}</h1>
+    </div>
+  );
+}
+
 export default function Features() {
   return (
-    <>
-      <div>
-        <div className=" mt-10">
-          <div className="header">
-            <div className="flex justify-between">
-              <img
-                src="./images/mainweb/3D/Galaxy 1.png"
-                alt=""
-                className="w-[40px]"
-              />
-              <div className="flex flex-col justify-center text-center">
-                <h1 className="text-2xl font-Ray-Black">
-                  ویژگی های شرکت{" "}
-                  <span className="text-main-blue-web">لاول کد</span>
-                </h1>
-                <span className="text-main-green-web text-sm">مختص شماست</span>
+    <div className="mt-10">
+      <div className="header">
+        <div className="flex justify-between">
+          <img
+            src="./images/mainweb/3D/Galaxy 1.png"
+            alt=""
+            className="w-[40px]"
+          />
+          <div className="flex flex-col justify-center text-center">
+            <h1 className="text-2xl font-Ray-Black">
+              ویژگی های شرکت <span className="text-main-blue-web">لاول کد</span>
+            </h1>
+            <span className="text-main-green-web text-sm">مختص شماست</span>
+          </div>
+          <img
+            src="./images/mainweb/3D/Galaxy 2.png"
+            alt=""
+            className="w-[50px]"
+          />
+        </div>
+      </div>
+      <div className="allMain text-center">
+        <div className="main xl:grid md:grid-cols-12 mt-10 container md:mr-[70px] xl:mr-[53px] 2xl:mr-[96px]">
+          <div className="right md:col-span-6 md:col-start-1 md:col-end-7">
+            <div className="box md:h-[540px] max-w-[580px] h-[500px] bg-[#FFEBD3] rounded-3xl flex justify-center flex-col items-center">
+              <div className="img max-w-[300px] h-[250px]">
+                <img src="./images/mainweb/3D/Sec4/5.png" alt="" />
               </div>
-
-              <img
-                src="./images/mainweb/3D/Galaxy 2.png"
-                alt=""
-                className="w-[50px]"
-              />
+              <div className="box2 bg-white md:w-[450px] md:h-[230px] w-[360px] h-[200px] rounded-3xl flex flex-col items-center gap-6 mt-[20px]">
+                <div className="mt-4">
+                  <h1 className="font-Ray-Black">
+                    درصد رضایت و آمار سایت{" "}
+                    <span className="text-main-blue-web">لاول کد </span> از زمان
+                    ایجاد آن
+                  </h1>
+                </div>
+                <div className="font-Ray-Bold">
+                  <span className="font-ANJOMANFANUM-MEDIUM text-sm">
+                    97.6 درصد
+                  </span>
+                  <span className="mr-[130px]">نرخ رضایت مشتریان</span>
+                  <hr className="w-[310px]" />
+                </div>
+                <div className="font-Ray-Bold">
+                  <span className="font-ANJOMANFANUM-MEDIUM text-sm">
+                    3000+
+                  </span>
+                  <span className="mr-[145px]">مشتری از سراسر ایران</span>
+                  <hr className="w-[310px]" />
+                </div>
+                <div className="font-Ray-Bold">
+                  <span className="font-ANJOMANFANUM-MEDIUM text-sm">
+                    50000+
+                  </span>
+                  <span className="mr-[110px]">کاربران فعال محصولات ما</span>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="allMain text-center md:mr-[100px] lg:mr-[0] xl:mr-[0]   ">
-            <div className="main xl:grid md:grid-cols-12 mt-10 container ">
-              <div className="right md:col-span-6 md:col-start-1 md:col-end-7 ">
-                <div className="box md:w-[580px] md:h-[540px] w-[410px] h-[500px] bg-[#FFEBD3]   rounded-3xl">
-                  <div className="img w-[300px] h-[250px]  md:mr-[150px] mr-[60px]">
-                    <img
-                      src="./images/mainweb/3D/Sec4/5.png"
-                      alt=""
-                      className=""
-                    />
-                  </div>
-
-                  <div className="box2 bg-white md:w-[450px] md:h-[230px] w-[380px] h-[200px]  md:mr-[70px] mr-[15px]  rounded-3xl flex flex-col items-center gap-6 mt-[30px]">
-                    <div className="mt-4">
-                      <h1 className="font-Ray-Black">
-                        درصد رضایت و آمار سایت  <span className="text-main-blue-web" >لاول کد  </span>  از زمان ایجاد آن
-                      </h1>
-                    </div>
-
-                    <div className="font-Ray-Bold">
-                      <span className="font-ANJOMANFANUM-MEDIUM text-sm"> 97.6 درصد</span>
-                      <span className="mr-[130px]">نرخ رضایت مشتریان </span>
-                      <hr className="w-[310px]" />
-                    </div>
-
-                    <div className="font-Ray-Bold">
-                      <span className="font-ANJOMANFANUM-MEDIUM  text-sm">3000+</span>
-                      <span className="mr-[145px]">مشتری از سراسر ایران</span>
-                      <hr className="w-[310px]" />
-                    </div>
-
-                    <div className="font-Ray-Bold">
-                      <span className="font-ANJOMANFANUM-MEDIUM  text-sm">50000+</span>
-                      <span className="mr-[110px]">
-                        کاربران فعال محصولات ما
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="left md:col-start-7 md:col-end-13  mt-[50px] md:mt-[50px] xl:mt-[0]">
-                <div className="flex flex-wrap gap-5">
-                  <div className="bg-white  md:w-[280px] md:h-[260px] w-[190px]  h-[200px] text-center rounded-2xl">
-                    <img
-                      src="./images/mainweb/3D/Sec4/1.png"
-                      alt=""
-                      className="md:w-[200px] w-[140px] md:mr-10 mr-[25px]"
-                    />
-                    <h1 className="font-Ray-ExtraBold md:text-xl text-lg">
-                      بهره مندی از متخصصین
-                    </h1>
-                  </div>
-
-                  <div className="bg-white  md:w-[280px] md:h-[260px] w-[190px] h-[200px] text-center rounded-2xl">
-                    <img
-                      src="./images/mainweb/3D/Sec4/2.png"
-                      alt=""
-                      className="md:w-[200px] w-[140px] md:mr-10 mr-[25px]"
-                    />
-                    <h1 className="font-Ray-ExtraBold md:text-xl text-lg">
-                      امنیت بالا
-                    </h1>
-                  </div>
-
-                  <div className="bg-white  md:w-[280px] md:h-[260px] w-[190px] h-[200px] text-center rounded-2xl">
-                    <img
-                      src="./images/mainweb/3D/Sec4/3.png"
-                      alt=""
-                      className="md:w-[200px] w-[140px] md:mr-10 mr-[25px]"
-                    />
-                    <h1 className="font-Ray-ExtraBold md:text-xl text-lg">
-                      پشتیبانی سریع
-                    </h1>
-                  </div>
-
-                  <div className="bg-white  md:w-[280px] md:h-[260px] w-[190px] h-[200px] text-center rounded-2xl">
-                    <img
-                      src="./images/mainweb/3D/Sec4/4.png"
-                      alt=""
-                      className="md:w-[200px] w-[140px] md:mr-10 mr-[25px]"
-                    />
-                    <h1 className="font-Ray-ExtraBold md:text-xl text-lg">
-                      متعدد به زمان
-                    </h1>
-                  </div>
-                </div>
-              </div>
+          <div className="left md:col-start-7 md:col-end-13 mt-[50px] md:mt-[50px] xl:mt-[0]">
+            <div className="flex flex-wrap gap-5">
+              <FeatureCard
+                imageSrc="./images/mainweb/3D/Sec4/1.png"
+                title="بهره مندی از متخصصین"
+              />
+              <FeatureCard
+                imageSrc="./images/mainweb/3D/Sec4/2.png"
+                title="امنیت بالا"
+              />
+              <FeatureCard
+                imageSrc="./images/mainweb/3D/Sec4/3.png"
+                title="پشتیبانی سریع"
+              />
+              <FeatureCard
+                imageSrc="./images/mainweb/3D/Sec4/4.png"
+                title="متعدد به زمان"
+              />
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- frontend/src/components/SocialNetworks/SocialNetworks.jsx
+++ frontend/src/components/SocialNetworks/SocialNetworks.jsx
@@ -1,76 +1,52 @@
 import React from "react";
 
+const socialNetworksData = [
+  {
+    title: "مارا در اینستاگرام دنبال کنید.",
+    buttonText: "دنبال کنید",
+    imageSrc: "./images/mainweb/3D/Sec5/Instagram1.png",
+    backgroundColor: "#f61231",
+  },
+  {
+    title: "ارسال پیام در تلگرام به لاول کد.",
+    buttonText: "ارسال پیام",
+    imageSrc: "./images/mainweb/3D/Sec5/telegram.png",
+    backgroundColor: "#5ab1eb",
+  },
+  {
+    title: "ارسال پیام در واتساپ به لاول کد.",
+    buttonText: "ارسال پیام",
+    imageSrc: "./images/mainweb/3D/Sec5/Whatsapp4.png",
+    backgroundColor: "#45b945",
+  },
+];
+
 export default function SocialNetworks() {
   return (
-    <>
-      <div>
-        <div className="container">
-          <div className="all mt-10 xl:flex xl:justify-between  md:mr-[200px] lg:mr-[0] xl:mr-[0] ">
-            <div className="box bg-[#f61231] w-[400px] xl:w-[365px] rounded-3xl h-[120px] flex  mt-[50px]">
-              <div className="pb w-[40%] m-[20px]  ">
-                <h1 className="text-white font-Ray-Bold">مارا در اینستاگرام دنبال کنید.</h1>
-                <button className="text-black bg-white w-[100px] h-[35px] rounded-xl mt-[5px] text-sm font-Ray-Bold">
-                  دنبال کنید
-                </button>
-              </div>
-              <div className="img flex w-[60%] ">
-                <img
-                  src="./images/mainweb/3D/Sec5/Instagram1.png"
-                  alt=""
-                  className="w-[150px] h-[150px] mr-[35px] xl:mr-[20px] -mt-[40px]"
-                />
-                <img
-                  src="./images/mainweb/3D/Sec5/Instagram1.png"
-                  alt=""
-                  className="w-[50px] h-[50px] mt-[40px]"
-                />
-              </div>
+    <div className="container">
+      <div className="all mt-10 xl:flex xl:justify-between xl:flex-row flex flex-col items-center lg:mr-[0] xl:mr-[0]">
+        {socialNetworksData.map((network, index) => (
+          <div
+            key={index}
+            className={`box bg-[${network.backgroundColor}] max-w-[385px] rounded-3xl h-[120px] flex mt-[50px]`}
+          >
+            <div className="pb w-[40%] m-[20px]">
+              <h1 className="text-white font-Ray-Bold">{network.title}</h1>
+              <button className="text-black bg-white w-[100px] h-[35px] rounded-xl mt-[5px] text-sm font-Ray-Bold">
+                {network.buttonText}
+              </button>
             </div>
-
-            <div className="box bg-main-blue-web w-[400px] xl:w-[365px] rounded-3xl h-[120px] flex  mt-[50px]">
-              <div className="pb w-[40%] m-[20px]  ">
-                <h1 className="text-white font-Ray-Bold">ارسال پیام در تلگرام به لاول کد.</h1>
-                <button className="text-black bg-white w-[100px] h-[35px] rounded-xl mt-[5px] text-sm font-Ray-Bold">
-                  ارسال پیام
-                </button>
-              </div>
-              <div className="img flex w-[60%] ">
-                <img
-                  src="./images/mainweb/3D/Sec5/telegram.png"
-                  alt=""
-                  className="w-[150px] h-[150px] mr-[35px] xl:mr-[20px] -mt-[40px]"
-                />
-                <img
-                  src="./images/mainweb/3D/Sec5/telegram.png"
-                  alt=""
-                  className="w-[50px] h-[50px] mt-[40px]"
-                />
-              </div>
-            </div>
-
-            <div className="box bg-[#45b945] w-[400px] xl:w-[365px] rounded-3xl h-[120px] flex  mt-[50px]">
-              <div className="pb w-[40%] m-[20px]  ">
-                <h1 className="text-white font-Ray-Bold">ارسال پیام در واتساپ به لاول کد.</h1>
-                <button className="text-black bg-white w-[100px] h-[35px] rounded-xl mt-[5px] text-sm font-Ray-Bold">
-                ارسال پیام
-                </button>
-              </div>
-              <div className="img flex w-[60%] ">
-                <img
-                  src="./images/mainweb/3D/Sec5/Whatsapp4.png"
-                  alt=""
-                  className="w-[150px] h-[150px] mr-[35px] xl:mr-[20px] -mt-[40px]"
-                />
-                <img
-                  src="./images/mainweb/3D/Sec5/Whatsapp4.png"
-                  alt=""
-                  className="w-[50px] h-[50px] mt-[40px]"
-                />
-              </div>
+            <div className="img flex w-[60%]">
+              <img
+                src={network.imageSrc}
+                alt=""
+                className="w-[150px] h-[150px] mr-[35px] xl:mr-[20px] -mt-[40px]"
+              />
+              <img src={network.imageSrc} alt="" className="w-[50px] h-[50px] mt-[40px]" />
             </div>
           </div>
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
