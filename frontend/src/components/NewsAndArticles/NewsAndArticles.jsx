0252f3380e1886c5c6abe90315ebd11138311169
mahdi delavar <mdelever77@gmail.com>
2023-10-08 22:05:42 +0330

    profiles updatad

--- frontend/src/components/NewsAndArticles/NewsAndArticles.jsx
+++ frontend/src/components/NewsAndArticles/NewsAndArticles.jsx
@@ -23,21 +23,3 @@ export default function NewsAndArticles() {
     </div>
   );
 }
-
-{
-  /* <div className="container w-full h-40 flex py-4 md:justify-center justify-start relative">
-        <div className="flex flex-col text-main-light-web md:items-center gap-2 items-start ">
-          <h3 className="font-Ray-ExtraBold text-xl md:text-2xl lg:text-[30px]  lg:mt-10 mt-4">
-            آخرین اخبار و مقالات
-          </h3>
-          <p className="font-Ray-Medium text-xs md:text-lg lg:text-xl hidden md:flex">
-            در بلاگ لاوکد کد شما به آرشیو اخبار در مورد دنیای سایت و تکنولوژی
-            دسترسی خواهید داشت.
-          </p>
-        </div>
-        <button className="flex absolute top-[4rem] -left-5 lg:top-16 lg:left-10 items-center justify-center gap-2 w-24 h-10 lg:w-28 lg:h-12 bg-white rounded-xl font-Ray-Light text-sm text-main-dark-text-web">
-          <span>رفتن به بلاگ</span>
-          <i className="bi bi-arrow-left-short text-lg mt-2"></i>
-        </button>
-      </div>*/
-}
