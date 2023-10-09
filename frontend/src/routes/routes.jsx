ba5fa040757018e31efff75933bf91d03bbac82c
[rezaeiar] <rezaeiar0@gmail.com>
2023-10-09 23:23:49 +0330

    fix: add about us and contact us routes

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,10 +1,14 @@
 import Home from "../pages/Home/Home"
 import Portfolio from "../pages/Portfolio/Portfolio"
 import Login from "../pages/Login/Login"
+import AboutUs from '../pages/AboutUs/AboutUs'
+import ContactUs from '../pages/ContactUs/ContactUs'
 
 const routes = [
     { path: '/', element: <Home /> },
     { path: '/portfolio', element: <Portfolio /> },
-    { path: '/login', element: <Login /> }
+    { path: '/login', element: <Login /> },
+    { path: '/about-us', element: <AboutUs /> },
+    { path: '/contact-us', element: <ContactUs /> }
 ]
 export default routes
