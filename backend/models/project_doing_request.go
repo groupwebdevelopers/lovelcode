7cf4cee3cb97519e3bf1061cf0e53b9ed3dba23d
yaghoubi-mn <yaghoubi.mn@gmail.com>
2023-10-10 13:03:16 +0330

    update ProjectDoingRequest struct

--- backend/models/project_doing_request.go
+++ backend/models/project_doing_request.go
@@ -5,12 +5,12 @@ import (
 )
 
 type ProjectDoingRequest struct{
-	ID uint64
-	UserID uint64
-	User User
-	Title string
-	Description string
-	SuggestedPrice uint
-	TimeCreated time.Time
-	TimeModified time.Time
+	ID uint64 `gorm:"primaryKey"`
+	UserID uint64 `gorm:"not null"`
+	User User `gorm:"not null"`
+	Title string `gorm:"not null"`
+	Description string `gorm:"not null"`
+	SuggestedPrice uint 
+	TimeCreated time.Time `gorm:"not null"`
+	TimeModified time.Time `gorm:"not null"`
 }
