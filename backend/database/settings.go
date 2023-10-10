268240465708c5f591182e3f53e1938eb548ea3d
yaghoubi-mn <yaghoubi.mn@gmail.com>
2023-10-10 10:46:39 +0330

    create settings file and fill it

---
+++ backend/database/settings.go
@@ -0,0 +1,23 @@
+package database
+
+
+type SettingsDB struct{
+	Key string `gorm:"unique,not null"`
+	Value string `gorm:"not null"`
+}
+
+var Settings map[string]string
+
+func (s *SettingsDB) SetupSettings() error{
+	var st []SettingsDB
+	if err:=DB.Find(&st).Error; err!=nil{
+		return err
+	}
+	
+	Settings = make(map[string]string)
+	for _, s := range st{
+		Settings[s.Key] = s.Value
+	}
+
+	return nil
+}
