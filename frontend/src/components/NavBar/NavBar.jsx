046757392db091c082020011a837098f9e18a411
mahdi delavar <mdelever77@gmail.com>
2023-10-10 15:38:09 +0330

    Contactus page added

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -50,7 +50,7 @@ export default function NavBar() {
                     </NavLink>
                 </li>
                 <li className='hover:text-main-blue-web transition-colors'>
-                    <NavLink to='/g' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
+                    <NavLink to='/contact-us' className={({ isActive }) => isActive ? "text-main-blue-web relative before:content-[''] before:absolute before:top-0 before:right-0 before:h-0.5 before:w-0.5 before:bg-main-blue-web" : ''}>
                         تماس با ما
                     </NavLink>
                 </li>
