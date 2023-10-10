605a48991ca5327111459e87a214bde9b608142b
mahdi delavar <mdelever77@gmail.com>
2023-10-10 18:25:06 +0330

    contactus pages styles updated

--- frontend/src/pages/ContactUs/ContactUs.jsx
+++ frontend/src/pages/ContactUs/ContactUs.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import SocialNetworks from "../../components/SocialNetworks/SocialNetworks";
 import Footer from "../../components/Footer/Footer";
+import { Link } from "react-router-dom";
 export default function ContactUs() {
   return (
     <div>
       <NavBar />
-      <div className="w-full container px-8 flex gap-3 items-center text-main-dark-text-web font-Ray-Bold text-xs md:text-sm">
-      <i class="bi bi-geo-alt"></i>
-      <span>خانه </span>
-      <i class="bi bi-chevron-left"></i>
-      <span>تماس با ما</span>
+      <div className="w-full container px-8 flex gap-1 items-center text-main-dark-text-web font-Ray-Bold text-xs md:text-sm">
+        <i class="bi bi-geo-alt text-xs"></i>
+        <Link to={"/"}>
+          <span className="cursor-pointer">خانه </span>
+        </Link>
+        <i class="bi bi-chevron-left text-xs"></i>
+        <span className="cursor-pointer">تماس با ما</span>
       </div>
       <div className="container w-full flex flex-col items-center">
         <h1 className="text-main-dark-text-web font-Ray-ExtraBold text-xl md:text-2xl lg:text-3xl">
@@ -44,7 +47,7 @@ export default function ContactUs() {
             className="col-span-12 p-4 h-52 rounded-xl outline-none font-Ray-Bold text-sm"
             placeholder="متن پیام"
           ></textarea>
-          <button className="w-32 h-12 rounded-xl bg-main-blue-web text-main-light-web mt-4 font-Ray-Bold text-sm">
+          <button className="w-32 h-12 rounded-xl bg-main-blue-web text-main-light-web mt-4 font-Ray-Bold text-sm flex justify-center items-center gap-2">
             <span>ثبت درخواست</span>
             <i className="bi bi-arrow-left-short text-lg mt-2"></i>
           </button>
@@ -53,8 +56,8 @@ export default function ContactUs() {
       <div className="container p-2 px-4  mt-14">
         <ul className="w-full flex justify-between md:px-[5rem] lg:px-[8rem] px-5 py-10 rounded-3xl bg-white flex-grow flex-wrap gap-y-4 gap-x-4">
           <li className=" flex flex-col items-start">
-            <div className="font-Ray-Bold text-main-blue-web text-lg flex gap-2">
-              <i class="bi bi-stopwatch"></i>
+            <div className="font-Ray-Bold text-main-blue-web text-lg flex gap-2 items-center">
+              <i class="bi bi-stopwatch text-base"></i>
               <span>ساعت کاری</span>
             </div>
             <span className="font-Ray-Bold text-sm mt-1">
@@ -62,8 +65,8 @@ export default function ContactUs() {
             </span>
           </li>
           <li className=" flex flex-col items-start">
-            <div className="font-Ray-Bold text-main-blue-web text-lg flex gap-2">
-              <i class="bi bi-envelope"></i>
+            <div className="font-Ray-Bold text-main-blue-web text-lg flex gap-2 items-center">
+              <i class="bi bi-envelope text-base"></i>
               <span>ایمیل</span>
             </div>
             <span className="font-Ray-Bold text-sm mt-1">
@@ -71,18 +74,20 @@ export default function ContactUs() {
             </span>
           </li>
           <li className=" flex flex-col items-start">
-            <div className="font-Ray-Bold text-main-blue-web text-lg flex gap-2">
-              <i class="bi bi-telephone-forward"></i>
+            <div className="font-Ray-Bold text-main-blue-web text-lg flex gap-2 items-center">
+              <i class="bi bi-telephone-forward text-base"></i>
               <span>شماره پشتیبان واتساپ</span>
             </div>
             <span className="font-Ray-Bold text-sm mt-1">091212121212</span>
           </li>
           <li className=" flex flex-col items-start">
-            <div className="font-Ray-Bold text-main-blue-web text-lg flex gap-2">
-              <i class="bi bi-geo-alt"></i>
+            <div className="font-Ray-Bold text-main-blue-web text-lg flex gap-2 items-center">
+              <i class="bi bi-geo-alt text-base"></i>
               <span>آدرس شرکت</span>
             </div>
-            <span className="font-Ray-Bold text-sm mt-1">---------------------</span>
+            <span className="font-Ray-Bold text-sm mt-1">
+              ---------------------
+            </span>
           </li>
         </ul>
       </div>
