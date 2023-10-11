ced2ee2d572c3d6c4237b40eae78398079594896
yaghoubi-mn <yaghoubi.mn@gmail.com>
2023-10-11 17:38:35 +0330

    add type CEPDR, create some functions

--- backend/models/project_doing_request.go
+++ backend/models/project_doing_request.go
@@ -14,3 +14,31 @@ type ProjectDoingRequest struct{
 	TimeCreated time.Time `gorm:"not null"`
 	TimeModified time.Time `gorm:"not null"`
 }
+
+// create or edit project doing request
+type CEPDR struct{
+	Title string `json:"title"`
+	Description string `json:"description"`
+	SuggestedPrice int `json:"suggestedprice`
+}
+
+func (pdr *ProjectDoingRequest) FillWithCEPDR(ce CEPDR){
+	pdr.Title = ce.Title
+	pdr.Description = ce.Description
+	if ce.SuggestedPrice >= 0{
+		pdr.SuggestedPrice = uint(ce.SuggestedPrice)
+	}
+}
+
+func (c CEPDR) Check() error{
+	if err:=utils.IsJustLetter(c.Title, "-!$%&*()_+= .,?");err!=nil{
+		return err
+	}
+	if c.Description == ""{
+		return errors.New("empty desciption")
+	}
+	if c. SuggestedPrice < 0{
+		return errors.New("negetive suggested price")
+	}
+	return nil
+}
