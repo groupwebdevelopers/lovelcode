9179652ed77753df80b7cf74ed35027f88d971c5
yaghoubi-mn <yaghoubi.mn@gmail.com>
2023-10-12 11:08:18 +0330

    update file

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 
-start CMD /k "cd frontend && npm run build && cd .. && cd backend && go mod tidy && go run ."
+start CMD /k "cd frontend && npm run build "
+start CMD /k "cd .. && cd backend && go env -w GOPROXY=direct && go mod tidy && go run ."
