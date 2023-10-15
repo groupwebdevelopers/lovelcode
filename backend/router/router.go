4145c3ecaef1a7d0b9ca775bf78b8cf7b873f382
yaghoubi-mn <yaghoubi.mn@gmail.com>
2023-10-15 14:33:38 +0330

    add some routes and comments

--- backend/router/router.go
+++ backend/router/router.go
@@ -10,27 +10,48 @@ func Route(app *fiber.App) {
 	apiV1 := apiOnly.Group("/v1")
 	apiV1.Get("/", handlers.Home)
 
+	fileUploadAuthReq := app.Group("/api/v1", handlers.AuthRequired)
+
+	authReq := apiV1.Group("/", handlers.AuthRequired)
+	adminReq := authReq.Group("/", handlers.AdminRequired)
+	
 	// auth := apiV1.Group("/auth")
 	apiV1.Post("/signin", handlers.Signin)
 	apiV1.Post("/signup", handlers.Signup)
-
-	authReq := apiV1.Group("/", handlers.AuthRequired)
-	authReq.Post("/pdr/create", handlers.CreateProjectDoingRequest)
-	authReq.Get("/pdr/get/:id", handlers.GetProjectDoingRequest)
-	authReq.Get("/pdr/get-all", handlers.GetAllProjectDoingRequests)
-	authReq.Post("/pdr/edit/:id", handlers.EditProjectDoingRequest)
+	adminReq.Post("/user/ban/:id", handlers.BanUser)
 	
-	authReq.Post("/user/ban/:id", handlers.BanUser)
-
-	authReq.Post("/plan/create", handlers.CreatePlan)
-
+	// Project Doing Request
+	adminReq.Post("/pdr/create", handlers.CreateProjectDoingRequest)
+	adminReq.Get("/pdr/get/:id", handlers.GetProjectDoingRequest)
+	adminReq.Get("/pdr/get-all", handlers.GetAllProjectDoingRequests)
+	adminReq.Put("/pdr/edit/:id", handlers.EditProjectDoingRequest)
+	adminReq.Delete("/pdr/delete/:id", handlers.DeleteProjectDoingRequest)
+	
+	// Plan
+	adminReq.Post("/plan/create", handlers.CreatePlan)
+	adminReq.Post("/plan/create-features/:planId", handlers.CreateFeatures)
+	fileUploadAuthReq.Post("/plan/upload-image/:planId", handlers.UploadPlanImage)
+	adminReq.Put("/plan/edit", handlers.EditPlan)
+	adminReq.Put("/plan/edit-feature/:featureId", handlers.EditFeature)
+	fileUploadAuthReq.Put("/plan/edit-image/:planId")
+	adminReq.Get("/plan/get-all-plans", handlers.GetAllPlans)
+	adminReq.Get("/plan/get-plan/:planId", handlers.GetPlan)
+	adminReq.Get("/plan/get-all-features/:planId", handlers.GetAllFeatures)
+	adminReq.Get("/plan/get-feature/:featureId", handlers.GetFeature)
+	adminReq.Get("/plan/get-all-plans-and-features", handlers.GetAllPlansAndFeatures)
+	adminReq.Delete("/plan/delete-plan/:planId", handlers.DeletePlan) // todo:image must deleted
+	adminReq.Delete("/plan/delete-feature/:featureId", handlers.DeleteFeature)
+	
+	// Static
 	app.Static("/", "../frontend/dist")
 	app.Static("*", "../frontend/dist/index.html")
 
+	// apt not found
 	apiOnly.Use(func (c *fiber.Ctx) error{
 		return c.Status(404).JSON(fiber.Map{"error":"page not found"})
 	})
 
+	// static not found
 	app.Use(func (c *fiber.Ctx) error{
 		return c.SendStatus(404)
 	})
