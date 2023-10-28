b42356817fb53d276f2c5b1c15c32db2081fdbee
mohammadreza <spd.mohammadreza@gmail.com>
2023-10-28 11:49:42 -0700

    mohammad

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
-import routes from './routes/routes'
-import { useRoutes } from 'react-router-dom'
-import MenuContext from './context/MenuContext'
-import Menu from './components/Menu/Menu'
-import Overlay from './components/Overlay/Overlay'
+import { useState } from "react";
+import routes from "./routes/routes";
+import { useRoutes } from "react-router-dom";
+import MenuContext from "./context/MenuContext";
+import Menu from "./components/Menu/Menu";
+import Overlay from "./components/Overlay/Overlay";
 
 function App() {
-    const router = useRoutes(routes)
-    const [menuDisplay, setMenuDisplay] = useState(false)
-    const menuDisplayHandler = (display) => {
-        setMenuDisplay(display)
-    }
-    return (
-        <MenuContext.Provider
-            value={{ menuDisplay, menuDisplayHandler }}
-        >
-            <Menu />
-            <Overlay />
-            {router}
-        </MenuContext.Provider>
-    )
+  const router = useRoutes(routes);
+  const [menuDisplay, setMenuDisplay] = useState(false);
+  const menuDisplayHandler = (display) => {
+    setMenuDisplay(display);
+  };
+  return (
+    <div className="bg-[#f5f8fa]">
+      <MenuContext.Provider value={{ menuDisplay, menuDisplayHandler }}>
+        <Menu />
+        <Overlay />
+        {router}
+      </MenuContext.Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
